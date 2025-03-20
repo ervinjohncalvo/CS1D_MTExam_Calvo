897c0d3943f76b2026509286d5e4fe1a1d5c171a
ervinjohncalvo <calvoervinjohn@gmail.com>
2025-03-20 19:37:25 +0800

    Exam

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+//Collect the following user information with the use of prompt()
+let fullName = prompt("Enter your full name (first and last):");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Log the collected information in the console
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
