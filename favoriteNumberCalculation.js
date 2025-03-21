fa8d8ac3036c91d9d911063b5a4f86bb2280fdd2
ervinjohncalvo <calvoervinjohn@gmail.com>
2025-03-21 23:35:54 +0800

    Exam

---
+++ favoriteNumberCalculation.js
@@ -0,0 +1,14 @@
+let theFavNumber = 10;
+let guess;
+
+while (guess != theFavNumber) {
+    guess = (prompt("Guess the favorite number: "), 10);
+
+    if (guess = theFavNumber) {
+        console.log("High!");
+    } else if (guess = theFavNumber) {
+        console.log("Low!");
+    } else {
+        console.log("Correct!");
+    }
+}
